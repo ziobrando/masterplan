e128eb9331cdee8d4e2961e83a2c2c1bfbad4b6e
Alberto Brandolini <alberto.brandolini@gmail.com>
2023-08-18 11:06:56 +0300

    Just embellished the code.

--- src/test/java/it/avanscoperta/masterplan/common/domain/PriorityTest.java
+++ src/test/java/it/avanscoperta/masterplan/common/domain/PriorityTest.java
@@ -1,18 +1,22 @@
 package it.avanscoperta.masterplan.common.domain;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayName("[MasterPlan | Common]: Priority Value Object")
 public class PriorityTest {
 
     @Test
+    @DisplayName("Can define a priority")
     public void canDefineAPriority() {
         Priority priority = new Priority("Label", 99);
         assertNotNull(priority);
     }
 
     @Test
+    @DisplayName("Priorities trump each other")
     public void prioritiesTrumpEachOther() {
         Priority weaker = new Priority("Weaker", 25);
         Priority stronger = new Priority("Stronger", 50);
@@ -22,6 +26,7 @@ public class PriorityTest {
     }
 
     @Test
+    @DisplayName("Same priority does not trump")
     public void samePriorityDoesNotTrump() {
         Priority original = new Priority("Same level", 50);
         Priority same = new Priority("Same level", 50);
@@ -30,16 +35,16 @@ public class PriorityTest {
     }
 
     @Test
+    @DisplayName("Only positive priorities are allowed")
     public void prioritiesArePositive() {
-        assertThrows(IllegalArgumentException.class, () -> {
-            Priority negative = new Priority("negative", -1);
-        });
+        assertThrows(IllegalArgumentException.class,
+                () -> new Priority("negative", -1));
     }
 
     @Test
+    @DisplayName("Priorities are below 100")
     public void prioritiesAreBelow100() {
-        assertThrows(IllegalArgumentException.class, () -> {
-            Priority exceeding = new Priority("exceeding", 101);
-        });
+        assertThrows(IllegalArgumentException.class,
+                () -> new Priority("exceeding", 101));
     }
 }
